7c887cb2759d76b03fae77e6b6471bdeabaa0c35
VSdGxmvZ <kov3372@gmail.com>
2021-11-08 17:30:13 +0200

    init branch home_task1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello from master");
+        System.out.println("hello from home_home_task1");
     }
 }
